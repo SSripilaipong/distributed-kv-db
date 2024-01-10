3edfa57d5829dcbb419ec1a2f5267010a4358e6e
SSripilaipong <SHSnail@mail.com>
2024-01-10 19:15:57 +0700

    refactor(serverside.db.coordinator.quorum.read): extract response.Channel()

--- serverside/db/coordinator/quorum/read/read_nodes.go
+++ serverside/db/coordinator/quorum/read/read_nodes.go
@@ -2,34 +2,16 @@ package read
 
 import (
 	"context"
-	"distributed-kv-db/common/chn"
-	"distributed-kv-db/common/fn"
-	"distributed-kv-db/common/grt"
 	"distributed-kv-db/common/rslt"
-	"distributed-kv-db/common/slc"
-	"distributed-kv-db/common/wgrp"
 	"distributed-kv-db/serverside/db/coordinator/quorum"
-	"sync"
+	"distributed-kv-db/serverside/db/coordinator/quorum/response"
 )
 
 func readNodesDataToChannel[Key, Data any, Node quorum.ReadNode[Key, Data]](ctx context.Context, key Key, nodes []Node) <-chan Data {
-	var wg sync.WaitGroup
-	wg.Add(len(nodes))
-	ch := make(chan Data, len(nodes))
-
-	slc.Do(grt.Do(readNodeDataToChannel[Key, Data, Node](ctx, &wg, ch, key)), nodes)
-	go chn.CloseAfterWaitGroup(&wg, ch)
-
-	return ch
+	return response.Channel(ctx, readNode[Key, Data, Node](ctx, key), nodes)
 }
 
-func readNodeDataToChannel[Key, Data any, Node quorum.ReadNode[Key, Data]](ctx context.Context, wg *sync.WaitGroup, ch chan<- Data, key Key) func(node Node) {
-	return fn.Do(wgrp.MustDone(wg, fn.Compose(
-		rslt.Fmap(chn.SendToWithContext(ctx, ch)), readNode[Data, Key, Node](ctx, key),
-	)))
-}
-
-func readNode[Data, Key any, Node quorum.ReadNode[Key, Data]](ctx context.Context, key Key) func(node Node) rslt.Of[Data] {
+func readNode[Key, Data any, Node quorum.ReadNode[Key, Data]](ctx context.Context, key Key) func(node Node) rslt.Of[Data] {
 	return func(node Node) rslt.Of[Data] {
 		return node.Read(ctx, key)
 	}
